036ad0071ca7e9087f5fe71b939ee5be513bf4e1
emmanuel <emmanjeru2002@gmail.com>
2024-12-07 12:12:58 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+ 
+  
+
+
+function countDown(){
+    let countDown = 10;
+    while (countDown > -1) {
+      console.log(countDown --);
+    }
+}
